9a92577f698f475114e0bf751b62618335f3b55b
Alwalaswarnaraj <allwalaswarnaraj541@gmail.com>
2025-07-22 14:21:18 +0530

    leaveType story partially completed

--- src/main/java/com/paves/employee_leave_management/serviceInterface/LeaveTypeServiceInterface.java
+++ src/main/java/com/paves/employee_leave_management/serviceInterface/LeaveTypeServiceInterface.java
@@ -1,4 +1,10 @@
 package com.paves.employee_leave_management.serviceInterface;
 
+import com.paves.employee_leave_management.entities.LeaveType;
+
+import java.util.List;
+
 public interface LeaveTypeServiceInterface {
+    public LeaveType addLeaveType(LeaveType leaveType);
+    public List<LeaveType> getAllLeaveTypes();
 }
