27979d8a41adc6c90d25452b726a2c66e194fd60
Ajay <kkajaykumar0313@gmail.com>
2025-09-19 12:51:18 +0530

    update

--- src/main/java/com/paves/employee_leave_management/service/LeaveRequestService.java
+++ src/main/java/com/paves/employee_leave_management/service/LeaveRequestService.java
@@ -792,7 +792,8 @@ public class LeaveRequestService implements LeaveRequestServiceInterface {
         LeaveRequestValidationDTO validationDTO = LeaveRequestValidationDTO.builder()
                 .leaveId(request.getLeaveId())
                 .employeeId(request.getEmployee().getEmployeeId())
-                .leaveTypeId(updatedLeaveType.getLeaveTypeId())
+//                .leaveTypeId(updatedLeaveType.getLeaveTypeId())
+                .leaveTypeId(updateRequest.getLeaveTypeId() != null ? updateRequest.getLeaveTypeId():updatedLeaveType.getLeaveTypeId())
 
                 // Use the new start date if provided, otherwise keep the old one
                 .startDate(updateRequest.getStartDate() != null ? updateRequest.getStartDate() : request.getStartDate())
@@ -807,6 +808,7 @@ public class LeaveRequestService implements LeaveRequestServiceInterface {
                 .reason(updateRequest.getReason() != null ? updateRequest.getReason() : request.getReason())
 
                 .driveLink(updateRequest.getDriveLink() != null ? updateRequest.getDriveLink() : request.getDriveLink())
+                .requestDate(request.getRequestDate())
                 .build();
 
         // Validate proposed update
@@ -826,7 +828,7 @@ public class LeaveRequestService implements LeaveRequestServiceInterface {
                 request.getEmployee().getEmployeeId(),
                 updateRequest.getLeaveTypeId(),
                 updateRequest.getDaysRequested(),
-                updateRequest.getRequestDate().getYear()
+                request.getRequestDate().getYear()
         );
 
         // Save only if there are changes
