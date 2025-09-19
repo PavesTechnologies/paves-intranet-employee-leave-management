c66015fccc575f8f142e218d3b0c1c11d0490ae4
Alwalaswarnaraj <allwalaswarnaraj541@gmail.com>
2025-09-19 09:07:43 +0530

    fixes

--- src/main/java/com/paves/employee_leave_management/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/paves/employee_leave_management/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.paves.employee_leave_management.globalExceptionHandler;
 
 
+import com.paves.employee_leave_management.dto.ApiResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,5 +20,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(LeaveBalanceExceptionHandler.class)
     public ResponseEntity<String> handleException(LeaveBalanceExceptionHandler leaveBalanceExceptionHandler) {
         return ResponseEntity.status(400).body(leaveBalanceExceptionHandler.getExMsg());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse<String>> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ApiResponse<>(false, "Duplicate entry: leave type already exists", null));
     }
 }
