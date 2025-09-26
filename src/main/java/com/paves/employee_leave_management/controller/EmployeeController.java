7692289559d1a6c9bf63f99cbcf9992877d1d634
Alwalaswarnaraj <allwalaswarnaraj541@gmail.com>
2025-09-26 11:32:27 +0530

    changes by swarna raj

--- src/main/java/com/paves/employee_leave_management/controller/EmployeeController.java
+++ src/main/java/com/paves/employee_leave_management/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
