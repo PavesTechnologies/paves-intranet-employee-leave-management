a381563e03fb8c7d634d54417d919319f2e332d5
Ajay <kkajaykumar0313@gmail.com>
2025-11-05 12:24:54 +0530

    holiday maker checker done

--- src/main/java/com/paves/employee_leave_management/controller/LeaveTypeController.java
+++ src/main/java/com/paves/employee_leave_management/controller/LeaveTypeController.java
@@ -10,6 +10,7 @@ import com.paves.employee_leave_management.enums.ApproverType;
 import com.paves.employee_leave_management.enums.LeaveTypesEnum;
 import com.paves.employee_leave_management.repo.EmployeeRepo;
 import com.paves.employee_leave_management.repo.LeaveTypeRepo;
+import com.paves.employee_leave_management.serviceInterface.ApprovalServiceInterface;
 import com.paves.employee_leave_management.serviceInterface.EmailServiceInterface;
 import com.paves.employee_leave_management.serviceInterface.LeaveTypeServiceInterface;
 import jakarta.validation.Valid;
@@ -36,7 +37,7 @@ public class LeaveTypeController {
     private LeaveTypeRepo leaveTypeRepo;
 
     @Autowired
-    private EmailServiceInterface.ApprovalService approvalService;
+    private ApprovalServiceInterface approvalService;
 
     @Autowired
     private EmployeeRepo employeeRepo;
@@ -93,7 +94,6 @@ public class LeaveTypeController {
                             null
                     ));
         }
-
 
 //        Optional<LeaveType> existingLeaveType = leaveTypeRepo.findByLeaveNameIgnoreCase(leaveType.getLeaveName());
 //        if (existingLeaveType.isPresent()) {
